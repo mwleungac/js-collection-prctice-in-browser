20abaec88a3db77702c6b6685b313b9ce0bd0b9b
mwleungac <57056434+mwleungac@users.noreply.github.com>
2020-04-03 09:47:27 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+    let sameElements = [];
+      collectionA.forEach(element => {
+    if (collectionB.includes(element)) {
+      sameElements.push(element);
+    }
+  });
+  return sameElements;
 }
