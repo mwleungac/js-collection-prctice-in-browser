58d2ce6524d96f5c60d90416e9982f276a415cd8
mwleungac <57056434+mwleungac@users.noreply.github.com>
2020-04-04 15:28:15 +0800

    Update practice-3.js

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,28 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let collectionC = countSameElements(collectionA);
+  
+  let objectValue = objectB.value;
+  collectionC.forEach(element => {
+    let elementKey = element.key;
+    if (objectValue.includes(elementKey)) {
+      element.count -= parseInt(element.count/3);
+    }
+  });
+  return collectionC;
+}
+
+
+function countSameElements(collection) {
+  let sameElements = [];
+  collection.forEach(element => {
+    let existingObj = sameElements.find(({ key }) => key === element);
+    if (!existingObj) {
+      sameElements.push({ key: element, count: 1 });
+    } else {
+      existingObj.count++;
+    }
+  });
+  return sameElements;
 }
